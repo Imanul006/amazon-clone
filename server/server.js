5ff361eded71a1b38e27f92a2b7ef4c8aa9f1124
Imanul006 <imanul006@gmail.com>
2022-02-14 03:08:19 +0530

    Database connection and user registration api

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+const User = require('./models/user')
+
+dotenv.config()
 
 const PORT = 3000
 const app = express()
+
+mongoose.connect(process.env.DATABASE_STRING, (err) => {
+    if (err) {
+        console.log("error : " + err)
+    } else {
+        console.log("Connected to the Database...")
+    }
+})
 
 // Middlewares
 app.use(morgan('dev'))
@@ -17,13 +31,23 @@ app.get("/", (req, res) => {
 
 // Post Requests
 app.post("/", (req, res) => {
-    res.json("Hello World!!!")
-    console.log(req.body)
+    let user = new User()
+    user.name = req.body.name
+    user.email = req.body.email
+    user.password = req.body.password
+
+    user.save((err) => {
+        if (err) {
+            res.json("error : " + err)
+        } else {
+            res.json("User Successfully Registered...")
+        }
+    })
 })
 
 app.listen(PORT, (err) => {
     if(err)
     console.log('ERROR: ' + err)
     else 
-    console.log('Server is running on PORT ' + PORT)
+    console.log('Server is running on PORT ' + PORT + '...')
 })
